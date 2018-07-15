0059d8fd7a5c13873f94ddbaf3b2eea9b3485237
Dev-C137 <dev.91096@gmail.com>
2018-07-15 20:29:36 +0530

    Added Working Functions

---
+++ main.py
@@ -0,0 +1,74 @@
+import requests
+import webbrowser
+from lomond import WebSocket
+import re
+import json
+import os
+
+
+def show_active():
+    main_url = 'https://api-quiz.hype.space/shows/now'
+    response_data = requests.get(main_url).json()
+    return response_data['active']
+
+
+def get_socket_url():
+    main_url = 'https://api-quiz.hype.space/shows/now'
+    response_data = requests.get(main_url).json()
+
+    socket_url = response_data['broadcast']['socketUrl'].replace('https', 'wss')
+    return socket_url
+
+
+def connect_websocket(socket_url, auth_token):
+    headers = {"Authorization": f"Bearer {auth_token}",
+               "x-hq-client": "Android/1.3.0"}
+
+    websocket = WebSocket(socket_url)
+
+    for header, value in headers.items():
+        websocket.add_header(str.encode(header), str.encode(value))
+
+    for msg in websocket.connect(ping_rate=5):
+        if msg.name == "text":
+            message = msg.text
+            message = re.sub(r"[\x00-\x1f\x7f-\x9f]", "", message)
+            message_data = json.loads(message)
+
+            if message_data['type'] == 'question':
+                question = message_data['type']
+
+                open_browser(question)
+
+
+def open_browser(question):
+
+    main_url = "https://www.google.co.in/search?q=" + question
+    webbrowser.open_new(main_url)
+
+
+def get_auth_token():
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "token.txt"), "r") as conn_settings:
+        settings = conn_settings.read().splitlines()
+        settings = [line for line in settings if line != "" and line != " "]
+
+        try:
+            auth_token = settings[0].split("=")[1]
+        except IndexError:
+            print('No Key is given!')
+            return 'NONE'
+
+        return auth_token
+
+
+if __name__ == '__main__':
+
+    input('Press enter to start : ')
+    if show_active():
+        url = get_socket_url()
+        print('Connecting to Socket : {}'.format(url))
+
+        token = get_auth_token()
+        if token == 'NONE':
+            print()
+        connect_websocket(url, token)
